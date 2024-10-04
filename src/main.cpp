a166515d34c77027e5f5ccf240360b5df32ec0d6
Shamiivan <shamiivan@gmail.com>
2024-10-04 17:06:07 -0400

    fix : cmake list

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include <raylib.h>
+
+#include <iostream>
 
 int main() {
-    const int screenWidth = 1600;
-    const int screenHeight = 900;
-
-    InitWindow(screenWidth, screenHeight, "IEEE C++ Workshop!");
-    SetTargetFPS(60);
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        // Where our game logic will go 
-        DrawRectangle(350, 250, 100, 100, RED);
-        DrawText("Hello, Snake!", 10, 10, 20, BLACK);
-        EndDrawing();
-    }
-    CloseWindow();
-    return 0;
+  std::cout << "Hello World!\n";
+  return 0;
 }
