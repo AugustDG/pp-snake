5a04bf812c882e79171b00935341f4bcc9e4a426
shamiivan <shamiivan@gmail.com>
2024-10-04 03:31:14 -0400

    Removed all files

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,20 @@
 #include "raylib.h"
-
-#include "settings.hpp"
-#include "snake.hpp"
-
-#include <game.hpp>
 #include <memory>
 
 int main() {
-    InitWindow(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, "pp snake - raylib");
+    const int screenWidth = 1600;
+    const int screenHeight = 900;
 
+    InitWindow(screenWidth, screenHeight, "IEEE C++ Workshop!");
     SetTargetFPS(60);
-
-    const auto initial_map_size = Vector2Int(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
-    const auto game_ptr = std::make_unique<Game>(BASE_MOVE_TIME, initial_map_size);
-
-    game_ptr->init();
-
     while (!WindowShouldClose()) {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
-
-        game_ptr->update();
-        game_ptr->render();
-
+        // Where our game logic will go 
+        DrawRectangle(350, 250, 100, 100, RED);
+        DrawText("Hello, Snake!", 10, 10, 20, BLACK);
         EndDrawing();
     }
-
     CloseWindow();
 
     return 0;
